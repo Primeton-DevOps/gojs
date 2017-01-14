f86731449937b35708c9792e87906471ef1a2f02
ZhongWen Li <lizhongwen1989@gmail.com>
2017-01-15 00:09:17 +0800

    Update

--- example/devops.js
+++ example/devops.js
@@ -1,7 +1,7 @@
-var __Go_GraphObject_Make = go.GraphObject.make;
+var $ = go.GraphObject.make;
 
 function init() {
-	window.myDiagram = __Go_GraphObject_Make(go.Diagram, "myDiagramDiv", {
+	window.myDiagram = $(go.Diagram, "myDiagramDiv", {
 		initialContentAlignment : go.Spot.TopCenter,
 		"undoManager.isEnabled" : true
 	});
@@ -28,7 +28,7 @@ function onNodeClick(e, node) {
 }
 
 function addLinkTemplatesToDiagram(myDiagram) {
-	var curveLink = __Go_GraphObject_Make(go.Link, {
+	var curveLink = $(go.Link, {
 		mouseEnter : onLinkMouseEnter,
 		mouseLeave : onLinkMouseLeave,
 		curve : go.Link.Bezier,
@@ -41,36 +41,36 @@ function addLinkTemplatesToDiagram(myDiagram) {
 		click : onLinkClick,
 		curviness : 20,
 		toolTip : // define a tooltip for each node
-		__Go_GraphObject_Make(go.Adornment, "Spot", {
+		$(go.Adornment, "Spot", {
 			background : "transparent"
 		}, // avoid hiding tooltip when mouse moves
-		__Go_GraphObject_Make(go.Placeholder, {
+		$(go.Placeholder, {
 			padding : 5
-		}), __Go_GraphObject_Make(go.TextBlock, {
+		}), $(go.TextBlock, {
 			alignment : go.Spot.Top,
 			alignmentFocus : go.Spot.Bottom,
 			stroke : "white",
 			font : "bold 16px sans-serif",
 			text : "See information"
 		}))
-	}, new go.Binding("points").makeTwoWay(), __Go_GraphObject_Make(go.Shape, {
+	}, new go.Binding("points").makeTwoWay(), $(go.Shape, {
 		strokeWidth : 1.5
 	}, new go.Binding("stroke", "isHighlighted", function(h) {
 		return h ? "crimson" : "green";
-	}).ofObject()), __Go_GraphObject_Make(go.Shape, // the arrowhead
+	}).ofObject()), $(go.Shape, // the arrowhead
 	{
 		toArrow : "standard",
 		stroke : null
 	}, new go.Binding("fill", "isHighlighted", function(h) {
 		return h ? "crimson" : "green";
 	}).ofObject())/*
-					 * , __Go_GraphObject_Make(go.Panel, "Auto",
-					 * __Go_GraphObject_Make(go.Shape, // the label background,
+					 * , $(go.Panel, "Auto",
+					 * $(go.Shape, // the label background,
 					 * which becomes // transparent around the edges { fill:
-					 * __Go_GraphObject_Make(go.Brush, "Radial", { 0: "rgb(240,
+					 * $(go.Brush, "Radial", { 0: "rgb(240,
 					 * 240, 240)", 0.3: "rgb(240, 240, 240)", 1: "rgba(240, 240,
 					 * 240, 0)" }), stroke: null }),
-					 * __Go_GraphObject_Make(go.TextBlock, "dependency", // the
+					 * $(go.TextBlock, "dependency", // the
 					 * label text { textAlign: "center", font: "9pt helvetica,
 					 * arial, sans-serif", margin: 4, editable: true // enable
 					 * in-place editing }, // editing the text automatically
@@ -79,21 +79,21 @@ function addLinkTemplatesToDiagram(myDiagram) {
 					 */
 	);
 
-	var orthogonalLink = __Go_GraphObject_Make(go.Link, {
+	var orthogonalLink = $(go.Link, {
 		mouseEnter : onLinkMouseEnter,
 		mouseLeave : onLinkMouseLeave,
 		routing : go.Link.Orthogonal
-	}, __Go_GraphObject_Make(go.Shape, new go.Binding("stroke",
+	}, $(go.Shape, new go.Binding("stroke",
 			"isHighlighted", function(h) {
 				return h ? "crimson" : "black";
-			}).ofObject()), __Go_GraphObject_Make(go.Shape, {
+			}).ofObject()), $(go.Shape, {
 		toArrow : "Standard",
 		stroke : null,
 	}, new go.Binding("fill", "isHighlighted", function(h) {
 		return h ? "crimson" : "black";
 	}).ofObject()));
 
-	var fieldLink = __Go_GraphObject_Make(go.Link, {
+	var fieldLink = $(go.Link, {
 		relinkableFrom : true,
 		relinkableTo : true, // let user reconnect links
 		toShortLength : 4,
@@ -101,9 +101,9 @@ function addLinkTemplatesToDiagram(myDiagram) {
 		routing : go.Link.Orthogonal
 	}, new go.Binding("routing", "datamap", function() {
 		return go.Link.Normal
-	}), __Go_GraphObject_Make(go.Shape, {
+	}), $(go.Shape, {
 		strokeWidth : 1.5
-	}), __Go_GraphObject_Make(go.Shape, {
+	}), $(go.Shape, {
 		toArrow : "Standard",
 		stroke : null
 	}));
@@ -119,11 +119,11 @@ function addLinkTemplatesToDiagram(myDiagram) {
 }
 
 function addGroupTemplatesToDiagram(myDiagram) {
-	var springGroup = __Go_GraphObject_Make(go.Group, "Auto", {
+	var springGroup = $(go.Group, "Auto", {
 		// width: 600,
 		// height: 300,
 		// alignment: go.Spot.Center,
-		layout : __Go_GraphObject_Make(go.ForceDirectedLayout, {
+		layout : $(go.ForceDirectedLayout, {
 		// defaultSpringLength: 30,
 		// defaultElectricalCharge: 100
 		})
@@ -132,19 +132,19 @@ function addGroupTemplatesToDiagram(myDiagram) {
 		 * .makeTwoWay(go.Point.stringify),
 		 */new go.Binding("desiredSize", "size",
 			go.Size.parse).makeTwoWay(go.Size.stringify),
-			__Go_GraphObject_Make(go.Shape, "Rectangle", {
+			$(go.Shape, "Rectangle", {
 				fill : "white",
 				stroke : "gray",
 				strokeWidth : 0
-			}), __Go_GraphObject_Make(go.Placeholder
+			}), $(go.Placeholder
 			// , {alignment: go.Spot.Center}
 			// , new go.Binding("desiredSize", "size",
 			// go.Size.parse).makeTwoWay(go.Size.stringify)
 			));
 
-	var rtg = __Go_GraphObject_Make(go.Group, "Auto", { // define the group's
+	var rtg = $(go.Group, "Auto", { // define the group's
 		// internal layout
-		layout : __Go_GraphObject_Make(go.TreeLayout, {
+		layout : $(go.TreeLayout, {
 			angle : 0,
 			arrangement : go.TreeLayout.ArrangementVertical,
 			isRealtime : false
@@ -154,11 +154,11 @@ function addGroupTemplatesToDiagram(myDiagram) {
 		 * .makeTwoWay(go.Point.stringify),
 		 */new go.Binding("desiredSize", "size",
 			go.Size.parse).makeTwoWay(go.Size.stringify),
-			__Go_GraphObject_Make(go.Shape, "Rectangle", {
+			$(go.Shape, "Rectangle", {
 				fill : "rgba(128,128,128,0.2)",
 				stroke : "gray",
 				strokeWidth : 0
-			}), __Go_GraphObject_Make(go.Placeholder));
+			}), $(go.Placeholder));
 
 	var groupTemplateMap = new go.Map("string", go.Group);
 
@@ -169,19 +169,19 @@ function addGroupTemplatesToDiagram(myDiagram) {
 }
 
 function addNodeTemplatesToDiagram(myDiagarm) {
-	var normalNode = __Go_GraphObject_Make(go.Node, "Auto", {
+	var normalNode = $(go.Node, "Auto", {
 		mouseEnter : onNodeMouseEnter,
 		mouseLeave : onNodeMouseLeave,
 		cursor : "pointer",
 		click : onNodeClick
-	}, __Go_GraphObject_Make(go.Panel, "Horizontal", __Go_GraphObject_Make(
+	}, $(go.Panel, "Horizontal", $(
 			go.Picture, {
 				margin : 5,
 				width : 50,
 				height : 50
 			}, new go.Binding("source") || "images/gear.svg"),
 
-	__Go_GraphObject_Make(go.TextBlock, "undefined", {
+	$(go.TextBlock, "undefined", {
 		margin : 12,
 		stroke : "white",
 		font : "14px sans-serif"
